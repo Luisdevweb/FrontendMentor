129a1077e25f88f606b711d79bce6b0f37e927b1
Luismac <luismachaca@outlook.com.pe>
2022-02-17 20:25:41 -0500

    fix validation after an error state

--- ping-coming-soon-page-master/js/scripts.js
+++ ping-coming-soon-page-master/js/scripts.js
@@ -11,7 +11,10 @@ const validationEmail = (email) => {
   const errorMessage = document.getElementById("error");
   const regex = /^[-\w.%+]{1,64}@(?:[A-Z0-9-]{1,63}\.){1,125}[A-Z]{2,63}$/i;
   if (!regex.test(email.value)) {
-    error.classList.add("error--visible");
+    errorMessage.classList.add("error--visible");
     email.classList.add("form__input--error");
+  } else {
+    errorMessage.classList.remove("error--visible");
+    email.classList.remove("form__input--error");
   }
 };
